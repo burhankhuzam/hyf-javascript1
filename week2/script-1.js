80df03da664c168ffd5294a8e1e2d271b086c1e6
Burhan Khuzam <burhankhuzam@gmail.com>
2018-10-27 20:02:43 +0200

    adding let

--- week2/script-1.js
+++ week2/script-1.js
@@ -1,13 +1,13 @@
 
-var message1 = "Hello World!";
+let message1 = "Hello World!";
 console.log(message1 + " - English");
 
 
-var message2 = "!مرحباً يا عالم";
+let message2 = "!مرحباً يا عالم";
 console.log(message2 + " - Arabic");
 
 
-var message3 = "Hallo wereld!";
+let message3 = "Hallo wereld!";
 console.log(message3 + " - Dutch");
 
 
