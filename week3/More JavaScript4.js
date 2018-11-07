5ab68323c78ee5bd35b5f4c20665efb4460ac4b6
Burhan Khuzam <burhankhuzam@gmail.com>
2018-11-07 15:25:15 +0100

    uploading 4 homeworks

---
+++ week3/More JavaScript4.js
@@ -0,0 +1,13 @@
+function vehicleType(color, object) {
+    if (color == "Blue" && object == 1){
+        console.log("a  " + color + " car")
+    } else if (color == "Red" && object == 2) {
+        console.log("a  " + color + " motorbike")
+    } else {
+        console.log("The object has no style")
+    }
+}
+vehicleType("Blue", 1)
+vehicleType("Red", 2)
+
+
